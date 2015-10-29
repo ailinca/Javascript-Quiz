6fe8826f02db07df4921d3fa43f6c3bce5e763ac
Andrei Ilinca <andrei.ilinca2405@aut.pub.ro>
2015-10-29 09:35:42 +0000

    Added images to json

--- json.js
+++ json.js
@@ -1,13 +1,14 @@
 data = {"quizcontent": [
-    {"question": "Cati membrii are formatia Parazitii?",  "correct": 2, "a1":"2", "a2":"3","a3":"4","a4":"sin(pi/2)"},
-    {"question": "Care este numele real al lui Freakadadisk?", "correct": 3, "a1":"Catalin Stefan Ion", "a2":"Bogdan Ionut Pastaca","a3":"Petre Urda","a4":"Vlad Irimia"},
-	  {"question": "Ce vrea sa dea Cheloo?", "correct": 3, "a1":"Bani la saraci", "a2":"Ortul popii","a3":"bani falsi de pomana","a4":"de baut"},
-	  {"question": "De unde ne dedica Ombladon aceasta balada?", "correct": 1, "a1":"din coltul blocului", "a2":"din Pantelimon","a3":"din una in alta","a4":"din parnaie"},
-    {"question": "Cine cred ca ti-a vorbit despre mine?",  "correct": 4, "a1":"Cheloo", "a2":"Mos Craciun","a3":"Sanziana Buruiana","a4":"ma-*a"},
-    {"question": "Pabibabumbumbambribum SA!", "correct": 2, "a1":"asa si?", "a2":"S cum?","a3":"pabibace?","a4":"n-am auzit de aceasta firma"},
-	  {"question": "Ce mai poti sa faci cu 2 palme?", "correct": 1, "a1":"ocolul pamantului in 4 labe", "a2":"sa aplauzi","a3":"flotari","a4":"sa aplauzi la spate"},
-	  {"question": "Din 10 vibratii, cate sunt negative?", "correct": 4, "a1":"nici una", "a2":"2","a3":"3.14","a4":"9"},
-    {"question": "Ce fel de semnale iti dau cei de la Parazitii sa ii vezi de departe?",  "correct": 3, "a1":"SMS", "a2":"bufnita","a3":"semnale cu fum","a4":"semnale armonice de frecventa joasa"},
-    {"question": "Unde propun Parazitii sa trimitem bulangii pachet?", "correct": 2, "a1":"Dubai", "a2":"Barcelona","a3":"Siria","a4":"Plescoi"}
+    {"question": "Cati membrii are formatia Parazitii?","image": "meta/intr1.jpg",  "correct": 2, "a1":"2", "a2":"3","a3":"4","a4":"sin(pi/2)"},
+    {"question": "Care este numele real al lui Freakadadisk?","image": "meta/intr2.jpg", "correct": 3, "a1":"Catalin Stefan Ion", "a2":"Bogdan Ionut Pastaca","a3":"Petre Urda","a4":"Vlad Irimia"},
+	{"question": "Ce vrea sa dea Cheloo?", "image": "meta/intr3.jpg", "correct": 3, "a1":"Bani la saraci", "a2":"Ortul popii","a3":"bani falsi de pomana","a4":"de baut"},
+	{"question": "De unde ne dedica Ombladon aceasta balada?","image": "meta/intr4.jpg", "correct": 1, "a1":"din coltul blocului", "a2":"din Pantelimon","a3":"din una in alta","a4":"din parnaie"},
+    {"question": "Cine cred ca ti-a vorbit despre mine?","image": "meta/intr5.jpg",  "correct": 4, "a1":"Cheloo", "a2":"Mos Craciun","a3":"Sanziana Buruiana","a4":"ma-*a"},
+    {"question": "Pabibabumbumbambribum SA!","image": "meta/intr6.jpg","correct": 2, "a1":"asa si?", "a2":"S cum?","a3":"pabibace?","a4":"n-am auzit de aceasta firma"},
+	{"question": "Ce mai poti sa faci cu 2 palme?", "image": "meta/intr7.jpg","correct": 1, "a1":"ocolul pamantului in 4 labe", "a2":"sa aplauzi","a3":"flotari","a4":"sa aplauzi la spate"},
+	{"question": "Din 10 vibratii, cate sunt negative?","image": "meta/intr8.jpg", "correct": 4, "a1":"nici una", "a2":"2","a3":"3.14","a4":"9"},
+    {"question": "Ce fel de semnale iti dau cei de la Parazitii sa ii vezi de departe?", "image": "meta/intr9.jpg", "correct": 3, "a1":"SMS", "a2":"bufnita","a3":"semnale cu fum","a4":"semnale armonice de frecventa joasa"},
+    {"question": "Unde propun Parazitii sa trimitem bulangii pachet?","image": "meta/intr10.jpg", "correct": 2, "a1":"Dubai", "a2":"Barcelona","a3":"Siria","a4":"Plescoi"}
     ],
+	
 };
